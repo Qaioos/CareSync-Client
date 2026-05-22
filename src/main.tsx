3b5b42d5c3095f5d81b3882f6392696744bbe6c7
Qaioos <legend442005@gmail.com>
2026-05-22 22:04:03 +0300

    feat: add the routiing for main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { AuthProvider } from './Context/AuthUser.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { AuthProvider } from "./Context/AuthUser.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AuthProvider>
-    <App />
-    </AuthProvider>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <BrowserRouter>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </AuthProvider>
+        </BrowserRouter>
+    </StrictMode>,
+);
