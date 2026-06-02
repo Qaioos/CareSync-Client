089f4d6dcb2e78ce5268b565c09f082c5369674d
Qaioos <legend442005@gmail.com>
2026-06-02 11:13:28 +0300

    Change the URL To localhost api / becuse the plane free is fineshed

--- src/Config/axios.ts
+++ src/Config/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export  const axiosInstance = axios.create({
-    baseURL:"https://luminous-cherry-91ced558b9.strapiapp.com/api",
+    /* baseURL:"https://luminous-cherry-91ced558b9.strapiapp.com/api", */
+    baseURL:'http://localhost:1337/api',
     headers:{
         "Content-Type": "application/json",
         "Accept":"application/json"
